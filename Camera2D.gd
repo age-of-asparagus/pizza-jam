3339b961cdd36919db60df6da711ceccdecc8dac
Tylere Couture <tylerecouture@gmail.com>
2023-07-25 10:26:54 -0700

    death on camera squish

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,13 +1,12 @@
 extends Camera2D
 
 var camera_start_position = Vector2(242,154)
-onready var player = get_parent().get_node("Player")
+onready var player : KinematicBody2D = get_parent().get_node("Player")
 var camera_move_speed = 1
 
 func _physics_process(delta):
 	global_position.x += camera_move_speed
-	
-	
+	global_position.y = player.global_position.y
 	
 	if player.global_position.y > 400:
 		die()
@@ -15,3 +14,7 @@ func _physics_process(delta):
 func die():
 	global_position = camera_start_position
 	player.global_position = get_parent().get_node("PlayerStartPosition").global_position
+
+
+func _on_StaticBody2D_body_entered(body):
+	die()
